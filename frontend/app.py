69f589bc0557a43f8c0ff00c0472480b054a4b13
Angel Bencosme <angelbencosme2022@gmail.com>
2024-12-02 15:07:58 -0500

    added sign-in that updates Users table with new user

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="yourpassword",
+    password="A445a445.123.525",
     database="ecom"
 )
 
@@ -34,6 +34,32 @@ def login():
     else:
         # Render the login page again with an error message
         return render_template('index.html', error="Invalid credentials. Please try again.")
+    
+    
+@app.route('/register_page')
+def register_page():
+    # Render the HTML form
+    return render_template('signUp.html')
+    
+@app.route('/register', methods=['POST'])
+
+def register():
+    # Get form data
+    first_name = request.form['first-name']
+    middle_name = request.form['middle-name']
+    last_name = request.form['last-name']
+    email = request.form['email']
+    password = request.form['password']
+
+    # Insert the user into the Users table
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO Users (FName, M_I , LName , UEmail, UPassword,URole) VALUES (%s, %s , %s, %s ,%s , %s)", (first_name ,middle_name, last_name, email, password , 'customer'))
+    db.commit()
+
+    return "Registration Successful!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
